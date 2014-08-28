e45234ff5dd7c524d129a4e6b4b0950d4ec33b1d
Matthew Adams <matthew@matthewadams.me>
2014-08-28 15:29:56 -0500

    removed unnecessary @RunWith anno on test class

--- 2014/src/test/java/com/springone2gx/sdc/demo/test/SensorReadingJavaConfigIT.java
+++ 2014/src/test/java/com/springone2gx/sdc/demo/test/SensorReadingJavaConfigIT.java
@@ -1,16 +1,13 @@
 package com.springone2gx.sdc.demo.test;
 
-import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.springone2gx.sdc.demo.domain.SensorReading;
 import com.springone2gx.sdc.demo.repo.SensorReadingRepository;
 import com.springone2gx.sdc.demo.test.support.AbstractIntegrationTestConfig;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class SensorReadingJavaConfigIT extends AbstractSensorReadingIT {
 
