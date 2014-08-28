b073903002d95d889d052970bd4ea6d6ba6bbb4f
Matthew Adams <matthew@matthewadams.me>
2014-08-28 15:27:44 -0500

    created abstract test parent in prep for xml config addition

--- 2014/src/test/java/com/springone2gx/sdc/demo/test/SensorReadingJavaConfigIT.java
+++ 2014/src/test/java/com/springone2gx/sdc/demo/test/SensorReadingJavaConfigIT.java
@@ -1,16 +1,6 @@
 package com.springone2gx.sdc.demo.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.springframework.data.cassandra.repository.support.BasicMapId.id;
-
-import java.util.Date;
-import java.util.UUID;
-
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 import org.springframework.test.context.ContextConfiguration;
@@ -19,11 +9,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.springone2gx.sdc.demo.domain.SensorReading;
 import com.springone2gx.sdc.demo.repo.SensorReadingRepository;
 import com.springone2gx.sdc.demo.test.support.AbstractIntegrationTestConfig;
-import com.springone2gx.sdc.demo.test.support.AbstractSpringDataEmbeddedCassandraIntegrationTest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-public class SensorReadingJavaConfigIT extends AbstractSpringDataEmbeddedCassandraIntegrationTest {
+public class SensorReadingJavaConfigIT extends AbstractSensorReadingIT {
 
 	@Configuration
 	@EnableCassandraRepositories(basePackageClasses = SensorReadingRepository.class)
@@ -32,27 +21,5 @@ public class SensorReadingJavaConfigIT extends AbstractSpringDataEmbeddedCassand
 		public String[] getEntityBasePackages() {
 			return new String[] { SensorReading.class.getPackage().getName() };
 		}
-	}
-
-	@Autowired SensorReadingRepository repo;
-
-	@Before
-	public void beforeEach() {
-		assertNotNull(repo);
-	}
-
-	@Test
-	public void test() {
-		String id = UUID.randomUUID().toString();
-		Date time = new Date();
-		String data = UUID.randomUUID().toString();
-		repo.save(new SensorReading(id, time, data));
-
-		SensorReading found = repo.findOne(id("sensorId", id).with("time", time));
-		assertNotNull(found);
-
-		assertEquals(id, found.getSensorId());
-		assertEquals(time, found.getTime());
-		assertEquals(data, found.getData());
 	}
 }
